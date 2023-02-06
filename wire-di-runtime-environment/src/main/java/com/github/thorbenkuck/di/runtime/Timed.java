b6b59eb49a226eecb2173e84d4ce50a08ad789cb
Thorben Kuck <thorben.kuck@gmail.com>
2023-02-06 20:20:09 +0100

    [1.0.0-alpha3-SNAPSHOT]: Added Timed Beautiful rendering

--- wire-di-runtime-environment/src/main/java/com/github/thorbenkuck/di/runtime/Timed.java
+++ wire-di-runtime-environment/src/main/java/com/github/thorbenkuck/di/runtime/Timed.java
@@ -3,6 +3,8 @@ package com.github.thorbenkuck.di.runtime;
 import com.google.common.base.Stopwatch;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Timed {
@@ -15,6 +17,18 @@ public class Timed {
 
 	private Timed(Duration duration) {
 		this(duration.toNanos());
+	}
+
+	public static final Map<TimeUnit, String> HUMAN_READABLE = new HashMap<>();
+
+	static {
+		HUMAN_READABLE.put(TimeUnit.DAYS, "d");
+		HUMAN_READABLE.put(TimeUnit.HOURS, "h");
+		HUMAN_READABLE.put(TimeUnit.MINUTES, "m");
+		HUMAN_READABLE.put(TimeUnit.SECONDS, "s");
+		HUMAN_READABLE.put(TimeUnit.MILLISECONDS, "ms");
+		HUMAN_READABLE.put(TimeUnit.MICROSECONDS, "\u33B2");
+		HUMAN_READABLE.put(TimeUnit.NANOSECONDS, "ns");
 	}
 
 	public Timed plus(Timed timed) {
@@ -43,5 +57,50 @@ public class Timed {
 
 	public long get(TimeUnit timeUnit) {
 		return timeUnit.convert(nanos, TimeUnit.NANOSECONDS);
+	}
+
+	public String toString(TimeUnit timeUnit) {
+		return get(timeUnit) + HUMAN_READABLE.get(timeUnit);
+	}
+
+	@Override
+	public String toString() {
+		long milliseconds = get(TimeUnit.MILLISECONDS);
+		if (milliseconds == 0){
+			long micro = get(TimeUnit.MICROSECONDS);
+			if(micro == 0) {
+				return get(TimeUnit.NANOSECONDS) + HUMAN_READABLE.get(TimeUnit.NANOSECONDS);
+			} else {
+				return micro + HUMAN_READABLE.get(TimeUnit.MICROSECONDS);
+			}
+		}
+		return new TimedRenderer()
+				.append(TimeUnit.MINUTES)
+				.append(TimeUnit.SECONDS)
+				.append(TimeUnit.MILLISECONDS)
+				.toString();
+	}
+
+	class TimedRenderer {
+		private final StringBuilder result = new StringBuilder();
+
+		public TimedRenderer append(TimeUnit timeUnit) {
+			long done = get(timeUnit);
+			if(done == 0) {
+				return this;
+			}
+
+			if(!result.isEmpty()) {
+				result.append(", ");
+			}
+
+			result.append(done).append(HUMAN_READABLE.get(timeUnit));
+			return this;
+		}
+
+		@Override
+		public String toString() {
+			return result.toString();
+		}
 	}
 }
